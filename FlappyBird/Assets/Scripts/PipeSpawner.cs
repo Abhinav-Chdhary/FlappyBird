1269f30c0cafcb5a2412b96fc1af4a79777b9ba0
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-01-19 13:14:03 +0530

    Spawner script done

---
+++ FlappyBird/Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject pipe; //to get the whole game object of Prefab
+    public float spawnRate = 1f; //to customize the frequency of spawns
+    public float minHeight = -1f; 
+    public float maxHeight = 1f; //to customize difficulty level of obstacles
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(spawnner), spawnRate, spawnRate);
+    }
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(spawnner));
+    }
+    private void spawnner()
+    {
+        GameObject pipes = Instantiate(pipe, transform.position, Quaternion.identity);
+        pipes.transform.position += Vector3.up*Random.Range(minHeight, maxHeight);
+    }
+}
