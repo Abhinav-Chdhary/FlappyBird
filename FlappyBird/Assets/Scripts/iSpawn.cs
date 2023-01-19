cce674044b04d2c28acd20f9103b30f464411a12
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-01-19 13:20:16 +0530

    Script import fixed and new GameObject made

--- FlappyBird/Assets/Scripts/iSpawn.cs
+++ FlappyBird/Assets/Scripts/iSpawn.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class iSpawn : MonoBehaviour
 {
     public GameObject pipe; //to get the whole game object of Prefab
     public float spawnRate = 1f; //to customize the frequency of spawns
-    public float minHeight = -1f; 
+    public float minHeight = -1f;
     public float maxHeight = 1f; //to customize difficulty level of obstacles
 
     private void OnEnable()
@@ -18,6 +18,6 @@ public class NewBehaviourScript : MonoBehaviour
     private void spawnner()
     {
         GameObject pipes = Instantiate(pipe, transform.position, Quaternion.identity);
-        pipes.transform.position += Vector3.up*Random.Range(minHeight, maxHeight);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
     }
 }
