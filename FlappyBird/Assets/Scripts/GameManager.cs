2593c8ba55f5100505b5c37da8e3eb2ca93930d7
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-01-20 11:56:15 +0530

    Update GameManager.cs

--- FlappyBird/Assets/Scripts/GameManager.cs
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -1,15 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private int score = 0;
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
 
+    private void Awake()
+    {
+        Application.targetFrameRate= 60;
+        Pause();
+    }
+    private void Play()
+    {
+        score= 0;
+        scoreText.text = score.ToString();
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale= 1f;
+        player.enabled= true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        foreach (Pipes pipe in pipes)
+        {
+            Destroy(pipe.gameObject);
+        }
+
+    }
+    private void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled= false;
+    }
     public void GameOver()
     {
-        Debug.Log("Game over\n");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        score = 0;
+        Pause();
     }
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }
